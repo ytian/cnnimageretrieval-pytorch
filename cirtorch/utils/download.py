af32dc4cbf8c1f91f06fa9bcf85598216b99be1e
tianyebj <tianyebj@gmail.com>
2020-09-11 16:37:48 +0800

    fix

--- cirtorch/utils/download.py
+++ cirtorch/utils/download.py
@@ -53,19 +53,19 @@ def download_test(data_dir):
                     src_file = os.path.join(src_dir, dl_file)
                     dst_file = os.path.join(dst_dir, dl_file)
                     print('>> Downloading dataset {} archive {}...'.format(dataset, dl_file))
-                    os.system('wget {} -O {}'.format(src_file, dst_file))
+                    os.system("wget {} -O '{}'".format(src_file, dst_file))
                     print('>> Extracting dataset {} archive {}...'.format(dataset, dl_file))
                     # create tmp folder
                     dst_dir_tmp = os.path.join(dst_dir, 'tmp')
-                    os.system('mkdir {}'.format(dst_dir_tmp))
+                    os.system("mkdir '{}'".format(dst_dir_tmp))
                     # extract in tmp folder
-                    os.system('tar -zxf {} -C {}'.format(dst_file, dst_dir_tmp))
+                    os.system("tar -zxf '{}' -C '{}'".format(dst_file, dst_dir_tmp))
                     # remove all (possible) subfolders by moving only files in dst_dir
-                    os.system('find {} -type f -exec mv -i {{}} {} \\;'.format(dst_dir_tmp, dst_dir))
+                    os.system("find '{}' -type f -exec mv -i {{}} '{}' \\;".format(dst_dir_tmp, dst_dir))
                     # remove tmp folder
-                    os.system('rm -rf {}'.format(dst_dir_tmp))
+                    os.system("rm -rf '{}'".format(dst_dir_tmp))
                     print('>> Extracted, deleting dataset {} archive {}...'.format(dataset, dl_file))
-                    os.system('rm {}'.format(dst_file))
+                    os.system("rm '{}'".format(dst_file))
 
             # for roxford and rparis just make sym links
             elif dataset == 'roxford5k' or dataset == 'rparis6k':
@@ -73,7 +73,7 @@ def download_test(data_dir):
                 dataset_old = dataset[1:]
                 dst_dir_old = os.path.join(datasets_dir, dataset_old, 'jpg')
                 os.mkdir(os.path.join(datasets_dir, dataset))
-                os.system('ln -s {} {}'.format(dst_dir_old, dst_dir))
+                os.system("ln -s '{}' '{}'".format(dst_dir_old, dst_dir))
                 print('>> Created symbolic link from {} jpg to {} jpg'.format(dataset_old, dataset))
 
 
@@ -84,7 +84,7 @@ def download_test(data_dir):
         gnd_dst_file = os.path.join(gnd_dst_dir, gnd_dl_file)
         if not os.path.exists(gnd_dst_file):
             print('>> Downloading dataset {} ground truth file...'.format(dataset))
-            os.system('wget {} -O {}'.format(gnd_src_file, gnd_dst_file))
+            os.system("wget {} -O '{}'".format(gnd_src_file, gnd_dst_file))
 
 
 def download_train(data_dir):
